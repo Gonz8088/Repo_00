2926fbd51ce0069f508d18726dd774033e648bd7
Gonzales <212709006@logon.ds.ge.com>
2019-05-21 17:12:21 -0500

    updated repo00

--- repo00/pgphysics.py
+++ repo00/pgphysics.py
@@ -1,5 +1,94 @@
 import math
 
+"""def ord_of_mag(x, prefix=None):
+    if prefix == "E":         # exa
+        return x * (10**18)
+    elif prefix == "P":      # peta
+        return x * (10**15)
+    elif prefix == "T":      # tera
+        return x * (10**12)
+    elif prefix == "G":      # giga
+        return x * (10**9)
+    elif prefix == "M":      # mega
+        return x * (10**6)
+    elif prefix == "k":      # kilo
+        return x * (10**3)
+    elif prefix == "h":     # hecto
+        return x * (10**2)
+    elif prefix == "da":      # deka
+        return x * (10**1)
+    elif prefix == None:
+        return x * (10**0)
+    elif prefix == "d":      # deci
+        return x * (10**(-1))
+    elif prefix == "c":     # centi
+        return x * (10**(-2))
+    elif prefix == "m":     # milli
+        return x * (10**(-3))
+    elif prefix == "u":     # micro
+        return x * (10**(-6))
+    elif prefix == "n":      # nano
+        return x * (10**(-9))
+    elif prefix == "p":      # pico
+        return x * (10**(-12))
+    elif prefix == "f":     # femto
+        return x * (10**(-15))
+    elif prefix == "a":      # atto
+        return x * (10**(-18))
+    else:
+        return NULL"""
+
+def unit_conversion(x, frm, to):
+    if isinstance(frm, str):
+        raise TypeError("Expecting Numeric Value")
+    # exa(E) --> *
+    if frm == 'E':
+        if to == '*':
+            return x * (10**18)
+    '''# TODO: m --> kilo(k)
+    if from == '*':
+        if to == 'k':
+            return (x * (1/1000))
+    # TODO: m --> centi(c)
+        elif to == 'c':
+            return (x * 100)
+    # TODO: m --> micro(u)
+        elif to == 'u':
+            return (x * 1000000)
+    # TODO: m --> nano(n)
+        elif to == 'n':
+            return (x * 1000000000)
+
+    # TODO: kilo(k) --> m
+    if from == 'k':
+        if to == None:
+            return (x * 1000)
+    # TODO: kilo(k) --> centi(c)
+        elif to == 'c':
+            return (x * 100000)
+
+    # TODO: centi(c)--> kilo(k)
+    if from == 'c':
+        if to == 'k':
+            return (x * 100000)
+    # TODO: centi(c) --> m
+        elif to == None:
+            return
+    # TODO: centi(c) --> micro(m)
+        elif to == 'm':
+            return
+    # TODO: centi(c) --> nano(n)
+        elif to == 'n':
+            return
+
+    # TODO: micro(u) --> centi(c)
+    if from == 'u':
+        if to == 'c':
+            return
+    # TODO: micro(u) --> nano(n)
+        elif to == 'n':
+            return'''
+
 def distance(p1: int, p2: int) -> float:
     """distance -- takes two points as tuples, and returns the distance between them"""
     return math.sqrt((math.pow((p2[0] - p1[0]), 2) + math.pow((p2[1] - p1[1]), 2)))
--- tests/test_pgphysics.py
+++ tests/test_pgphysics.py
@@ -1,8 +1,20 @@
-import math
-
-def distance(p1, p2):
-    """distance -- takes two points as tuples, and returns the distance between them"""
-    return math.sqrt((math.pow((p2[0] - p1[0]), 2) + math.pow((p2[1] - p1[1]), 2)))
+import math, pytest
+from Repo_00.repo00.pgphysics import distance, unit_conversion
 
 def test_dist():
     assert distance((0,0), (5,5)) == 7.0710678118654755
+    assert distance((0,0), (0,5)) == 5
+    assert distance((0,0), (2,0)) == 2
+    assert distance((0,0), (1,2)) == 2.23606797749979
+    assert distance((0,0), (0,2)) == 2
+    assert distance((0,0), (-1,2)) == 2.23606797749979
+    assert distance((0,0), (-2,0)) == 2
+    assert distance((0,0), (-1,-2)) == 2.23606797749979
+    assert distance((0,0), (0,-2)) == 2
+    assert distance((0,0), (1,-2)) == 2.23606797749979
+
+def test_unit_conversion():
+    assert unit_conversion(-5, 'E', '*') == -5000000000000000000
+    assert unit_conversion(0, 'E', '*') == 0
+    assert unit_conversion(5, 'E', '*') == 5000000000000000000
+    assert unit_conversion('X', 'E', '*') == TypeError("Expecting Numeric Value")
